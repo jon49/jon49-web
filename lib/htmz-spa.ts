fc8ef5a537c16b7527f156a2fb08e5a649a92a3d
Jon Nyman <nymanjon@gmail.com>
2024-11-05 09:12:29 -0700

    Add htmz spa capability.

---
+++ lib/htmz-spa.ts
@@ -0,0 +1,18 @@
+let iframe = document.createElement("iframe")
+iframe.name = "htmz"
+iframe.hidden = true
+iframe.setAttribute("hf-select", "title,#head,#nav-main,main,#errors,#toasts,#scripts")
+document.body.appendChild(iframe)
+iframe.onload =
+function htmz(event: Event) {
+    let frame = event.target as HTMLIFrameElement
+    setTimeout(() => {
+        let dom = (frame.contentDocument?.querySelectorAll("head>template")[0] as HTMLTemplateElement)?.content
+        if (!dom) return
+        let select = frame.getAttribute("hf-select")
+        if (!select) return
+        // @ts-ignore
+        window.htmf.selectSwap(select, dom, true)
+    });
+}
+
