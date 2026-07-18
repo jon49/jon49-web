9ec3593f963c6fae9692e12e9d885e1548ecd536
Jon Nyman <nymanjon@gmail.com>
2026-07-18 16:56:30 -0700

    Update lifecycle to handle element creation.

--- lib/_action.lifecycle.ts
+++ lib/_action.lifecycle.ts
@@ -1,30 +1,30 @@
 export interface Disconnectable {
-  disconnectedCallback: () => void
+    disconnectedCallback: () => void
 }
 
 class DisconnectWatcher {
-  registry = new Map<HTMLElement, Disconnectable>()
+    registry = new Map<HTMLElement, Disconnectable>()
 
-  constructor() {
-    let observer = new MutationObserver(mutations => {
-      if (this.registry.size === 0) return
-      for (let mutation of mutations) {
-        for (let node of mutation.removedNodes) {
-          for (let [el, instance] of this.registry) {
-            if (node.contains(el)) {
-              this.registry.delete(el)
-              instance.disconnectedCallback()
+    constructor() {
+        let observer = new MutationObserver(mutations => {
+            if (this.registry.size === 0) return
+            for (let mutation of mutations) {
+                for (let node of mutation.removedNodes) {
+                    for (let [el, instance] of this.registry) {
+                        if (node.contains(el)) {
+                            this.registry.delete(el)
+                            instance.disconnectedCallback()
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    })
-    observer.observe(document.body, { childList: true, subtree: true })
-  }
+        })
+        observer.observe(document.body, { childList: true, subtree: true })
+    }
 
-  register(el: HTMLElement, instance: Disconnectable) {
-    this.registry.set(el, instance)
-  }
+    register(el: HTMLElement, instance: Disconnectable) {
+        this.registry.set(el, instance)
+    }
 }
 
 let watcher = new DisconnectWatcher()
@@ -32,60 +32,69 @@ let watcher = new DisconnectWatcher()
 const doc = document, w = window
 
 for (let event of ["click", "change", "submit"]) {
-  doc.addEventListener(event, e => {
-    let target = e.target
-    let action = findAttr(target, event)
-    if (!action) return
+    doc.addEventListener(event, e => {
+        let target = e.target
+        let action = findAttr(target, event)
+        if (!action) return
 
-    // @ts-ignore
-    handleCall(e, target, action)
-  })
+        // @ts-ignore
+        handleCall(e, target, action)
+    })
 }
 
 function handleLoad() {
-  for (let el of doc.querySelectorAll("[_load]")) {
-    let action = el.getAttribute("_load")
-    if (!action) continue
-    // @ts-ignore
-    handleCall(new Event("load"), el, action)
-    el.removeAttribute("_load")
-  }
+    for (let el of doc.querySelectorAll("[_load]")) {
+        let action = el.getAttribute("_load")
+        if (!action) continue
+        // @ts-ignore
+        handleCall(new Event("load"), el, action)
+        el.removeAttribute("_load")
+    }
 }
 
 doc.addEventListener("hz:completed", handleLoad)
 w.addEventListener("load", handleLoad)
 
 function findAttr(target: EventTarget | null, attr: string): string | null | undefined {
-  if (!target) return
-  // @ts-ignore
-  let result = target?.closest?.(`[_${attr}]`)?.getAttribute(`_${attr}`)
-  if (!result) {
+    if (!target) return
     // @ts-ignore
-    return findAttr(target?.form, attr)
-  }
-  return result
+    let result = target?.closest?.(`[_${attr}]`)?.getAttribute(`_${attr}`)
+    if (!result) {
+        // @ts-ignore
+        return findAttr(target?.form, attr)
+    }
+    return result
 }
 
+let hasLoaded: WeakMap<HTMLElement, string[]> = new WeakMap()
+
 function handleCall(
-  e: Event,
-  target: HTMLElement,
-  action: string): void {
-  // @ts-ignore
-  let form = target.tagName === "FORM" ? target : target.form
+    e: Event,
+    target: HTMLElement,
+    action: string): void {
+    // @ts-ignore
+    let form = target.tagName === "FORM" ? target : target.form
 
-  let actions = action.split(" ")
+    let actions = action.split(" ")
 
-  for (let action of actions) {
-    let fn: Function | undefined
+    for (let action of actions) {
+        let fn: Function | undefined
 
-    fn = w.app?.[action]
-    if (fn) {
-      let result = fn.call(null, { app: w.app, ev: e, target, el: target, form })
-      if (result?.disconnectedCallback) {
-        watcher.register(target, result as Disconnectable)
-      }
-    } else {
-      console.warn(`ACTION: Could not find function ${action}. Target element`, target)
+        fn = w.app?.[action]
+        if (fn) {
+            if (e.type === "load") {
+                if (!hasLoaded.get(target)?.includes(action)) {
+                    let result = fn.call(null, { app: w.app, ev: e, target, el: target, form })
+                    hasLoaded.get(target)?.push(action) || hasLoaded.set(target, [action])
+                    if (result.disconnectedCallback) {
+                        watcher.register(target, result as Disconnectable)
+                    }
+                }
+            } else {
+                fn.call(null, { app: w.app, ev: e, target, el: target, form })
+            }
+        } else {
+            console.warn(`ACTION: Could not find function ${action}. Target element`, target)
+        }
     }
-  }
 }
